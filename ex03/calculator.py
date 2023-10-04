ebabe0b9c07fda908793d4f650b4848ffed9805f
ghostyghast <antoinethebestgamer@gmail.com>
2023-10-04 15:54:39 +0800

    all files ready for submission

--- ex03/calculator.py
+++ ex03/calculator.py
@@ -20,14 +20,3 @@ class calculator:
         assert object != 0, "can't divide by 0"
         self.nums = [x / object for x in self.nums]
         print(self.nums)
-
-
-# v1 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
-# v1 + 5
-# print("---")
-# v2 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
-# v2 * 5
-# print("---")
-# v3 = calculator([10.0, 15.0, 20.0])
-# v3 - 5
-# v3 / 5
--- ex04/ft_calculator.py
+++ ex04/ft_calculator.py
@@ -11,10 +11,3 @@ class calculator:
     @staticmethod
     def sous_vec(V1: list[float], V2: list[float]) -> None:
         print('Sous vector is ', [float(x-y) for x, y in zip(V1, V2)])
-
-
-# a = [5, 10, 2]
-# b = [2, 4, 3]
-# calculator.dotproduct(a, b)
-# calculator.add_vec(a, b)
-# calculator.sous_vec(a, b)
