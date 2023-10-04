d1b16bf55f89e3c434a4f073bda83352c7118912
ghostyghast <antoinethebestgamer@gmail.com>
2023-10-04 13:09:02 +0800

    finished module

--- ex04/ft_calculator.py
+++ ex04/ft_calculator.py
@@ -1,12 +1,20 @@
 class calculator:
-#your code here
 
-    # decorator
+    @staticmethod
     def dotproduct(V1: list[float], V2: list[float]) -> None:
-        #your code here
-    # decorator
+        print('Dot product is:', sum([float(x*y) for x, y in zip(V1, V2)]))
+
+    @staticmethod
     def add_vec(V1: list[float], V2: list[float]) -> None:
-          #your code here
-    # decorator
+        print('Add vector is:', [float(x*y) for x, y in zip(V1, V2)])
+
+    @staticmethod
     def sous_vec(V1: list[float], V2: list[float]) -> None:
-         #your code here
+        print('Sous vector is ', [float(x-y) for x, y in zip(V1, V2)])
+
+
+# a = [5, 10, 2]
+# b = [2, 4, 3]
+# calculator.dotproduct(a, b)
+# calculator.add_vec(a, b)
+# calculator.sous_vec(a, b)
